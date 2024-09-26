e179fe7de1adb3ce1205f17a909a7e26f8afcf4f
Vikas Shete <Vikas Shete@DESKTOP-UL8GGQI>
2024-09-26 12:05:47 +0530

    by vikas

---
+++ GitDemo/src/com/cjcgit1/Push_pull_merge.java
@@ -0,0 +1,13 @@
+package com.cjcgit1;
+
+public class Push_pull_merge
+{
+public static void main(String[] args) 
+{
+System.out.println("Main Method");	
+}
+public void m1()
+{
+	System.out.println("m1 method");
+}
+}
